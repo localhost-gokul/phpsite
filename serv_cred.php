d96a5a905d4f075737867ce100de025684a46a28
gokul <devmsf7@gmail.com>
2024-09-08 00:52:23 +0530

    change the SQL.php to serv_cred

---
+++ serv_cred.php
@@ -0,0 +1,13 @@
+<?php
+$hostname ="localhost";   //server name
+$username ="root";        //MSYQL username
+$password ="";            //MYSQL password
+$dbname = "student_db";   //database name
+
+//initializing the connection with MYSQL
+$conn = new mysqli($hostname, $username, $password);
+
+if($conn ->connect_errno){
+    die("connection failed: ".$conn->connect_errno);
+}
+?>
